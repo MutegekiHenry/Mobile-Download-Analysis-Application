e6cf914c13860fed12f506fd9c9322682ed88891
engbenard <engbenard123@gmail.com>
2019-07-18 02:16:34 -0800

    Summary,bar plot and pie chart

--- server.R
+++ server.R
@@ -1,5 +1,9 @@
+#library(plotly)
 function(input, output){
- 
+  googleplaystore <- read.csv("D:/BEN/R Data Analysis/Software and sample data/googleplaystore.csv")
+  df<-data.frame(googleplaystore) 
+  
+  #Renders the menu in the sidebar
   output$menuItem <-renderMenu({
     menuItem("Home")
     menuItem("View Dataset")
@@ -7,11 +11,62 @@ function(input, output){
     menuItem("Plot")
     menuItem("Inferences")
     menuItem("Help")
+    })
+  
+  #Prints overall summary
+  output$summary <- renderPrint({
+    summary(googleplaystore)
+  })
+  
+  #Print summary by selection of the variable name
+  output$summary1<-renderPrint({
+    if(input$variable=="Category"){
+     print(summary(googleplaystore$Category)
+     )
+    }
+    if(input$variable=="Rating"){
+      print(summary(googleplaystore$Rating)
+      )
+    }
+    if(input$variable=="Reviews"){
+      print(summary(googleplaystore$Reviews)
+      )
+    }
+    if(input$variable=="Size"){
+      print(summary(googleplaystore$Size)
+      )
+    }
+    if(input$variable=="Installs"){
+      print(summary(googleplaystore$Installs)
+      )
+    }
+    if(input$variable=="Last.Updated"){
+      print(summary(googleplaystore$Last.Updated)
+      )
+    }
+    if(input$variable=="Price"){
+      print(summary(googleplaystore$Price)
+      )
+    }
+    if(input$variable=="Content.Rating"){
+      print(summary(googleplaystore$Content.Rating)
+      )
+    }
+    if(input$variable=="Current.Ver"){
+      print(summary(googleplaystore$Current.Ver)
+      )
+    }
     
+  })
+  
+  #Bar Plot
+  output$plot<-renderPlot({
+    plot(googleplaystore$Price)
   })
   
   
   
-  
-  
 }
+
+
+  
--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ dashboardPage(
   dashboardSidebar(
     sidebarMenu(
     menuItem("Home", tabName="Home"),
-    menuItem("View Dataset", tabName="View Dataset"),
+    menuItem("Dataset", tabName="Dataset"),
     menuItem("Summary", tabName="Summary"),
     menuItem("Plot", tabName="Plot"),
     menuItem("Inferences", tabName="Inferences"),
@@ -25,20 +25,91 @@ dashboardPage(
                h3("MDA-App is data system that analyzes app details from Google Play Store. 
                 It generates insights which are intended to help developers get more people to download their applications.
                 This system analyses how different application features affect the download rate of an application on the store.
-                The app details can be visualized giving a clear understanding of the data to the users.")),
-    tabItem(tabName="View Dataset",
+                The app details can be visualized giving a clear understanding of the data to the users.")
+            ),
+   
+     tabItem(tabName="Dataset",
             h2("Dataset")),
+    
     tabItem(tabName = "Summary",
-            h2("Summary Contents")),
+            h2("Overall Summary"),
+            #Code for summary
+            
+            fluidPage(
+              fluidRow(
+                box(title = "Overall Summary",
+                    status = "primary",
+                    solidHeader = T,
+                    collapsible = TRUE,
+                    "App details according to variable name",
+                    verbatimTextOutput("summary")
+                ),
+                box(title = "Summary Description",
+                    status = "primary",
+                    solidHeader = T,
+                    collapsible = TRUE,
+                    h3("Description of Summary"),
+                    h4("What Information does the summary represent: Summary is a function that produces different summaries
+                       of the several model fit functions. Some of these invoke methods
+                       which depend on the class. Mean, Median,Min,Max,1st Quartile and 3rd Quartile
+                       are some of the values that are displayed along with
+                       the indivudual statistics of the data we are analyzing. Some of the samaries shown in the
+                       left of this particular box include the classification stats by content rating, by reviews, 
+                       by category and so on as we explore in the boxes below to assess individual column summaries"))
+                    
+            
+    )
+    ),
+    
+  #summary by column
+    fluidRow(
+      box(title = "App Data Column",
+          status = "primary",
+          solidHeader = T,
+          collapsible = TRUE,
+          "To view summary, select a variable below",
+          selectInput("variable","Choose Variable",c("Category","Rating","Reviews","Size",
+                                                     "Installs","Type","Content.Rating","Genres",
+                                                     "Last.Updated"),selected = "Category")
+          
+          ),
+      #Box for displaying individual column summary
+      box(title = "Individual columns",
+          status = "primary",
+          solidHeader = T,
+          collapsible = TRUE,
+          verbatimTextOutput("summary1")
+      )
+    )
+    ),
+    
     tabItem(tabName="Plot",
-            h2("Plot contents")),
+            h2("Plot contents"),
+            fluidRow(
+              box(
+                title = "Category",
+                sliderInput("obs","No of Observations",
+                            min = 1,
+                            max = 10,
+                            value = 1,
+                            label = "Box plot for category"),
+                status = "primary",
+                solidHeader = TRUE, 
+                collapsible = TRUE,
+                plotOutput("plot")
+              )
+              
+            )
+            
+            ),
+    
     tabItem(tabName = "Inferences",
             h2("Inferences Contents")),
+    
     tabItem(tabName="Help",
             h2("Help contents"))
-    
-  )
-  )
   
+  )
+   )
   
 )
